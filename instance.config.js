964230072384e06735054d07569778b2dc797ac9
Rial_21 <67832384+Annamaria9-coder@users.noreply.github.com>
2021-06-01 01:08:32 +0300

    Update instance.config.js

--- instance.config.js
+++ instance.config.js
@@ -5,6 +5,6 @@ module.exports = {
   secondColorFG: '#FFFFFF',
   loadingColor: '#FFCC00',
   slug: process.env.COSMIC_BUCKET || 'music-site',
-  read_key: process.env.COSMIC_READ_KEY || '',
-  write_key: process.env.COSMIC_WRITE_KEY || ''
+  read_key: process.env.COSMIC_READ_KEY || 'L9i6FWcnuJdFvGp9IZRR9SJEf5is7j4OBf2zL0ynElgAGIrcUM',
+  write_key: process.env.COSMIC_WRITE_KEY || 'EdR2K071x0PxU966lQevjytJ8ZWki6r9hvKQPMqrw3kc6H9De9'
 }
